f659aaf869fe2c4492a0556dad38d47274098701
Brian Young <by12380@gmail.com>
2024-09-30 04:38:27 +0000

    process videos with task queues

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, flash, render_template, redirect, request
-from tasks import add
+from tasks import process_video_job
+from scripts import ensure_test_urls_in_supabase
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY', "super-secret")
@@ -11,10 +12,18 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/add', methods=['POST'])
-def add_inputs():
-    x = int(request.form['x'] or 0)
-    y = int(request.form['y'] or 0)
-    add.delay(x, y)
-    flash("Your addition job has been submitted.")
+@app.route('/batch-process', methods=['POST'])
+def batch_process():
+    urls = request.form['urls'].split('\r\n')
+    ensure_test_urls_in_supabase(urls)
+
+    task_ids = []
+
+    for url in urls:
+        response = process_video_job.delay(url)
+        task_ids.append(response.id)
+
+    task_ids_joined = "\n".join(task_ids)
+    flash(f"Your addition job has been submitted.\n{task_ids_joined}")
+
     return redirect('/')
--- tasks.py
+++ tasks.py
@@ -1,12 +1,32 @@
 import os
 from celery import Celery
 from celery.utils.log import get_task_logger
+from scripts import download_video, process_video, update_supabase
 
 app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
 logger = get_task_logger(__name__)
 
 
 @app.task
-def add(x, y):
-    logger.info(f'Adding {x} + {y}')
-    return x + y
+async def process_video_job(url):
+    video_path = ""
+    clips = []
+
+    try:
+        video_path, video_id = await download_video(url)
+        if video_path:
+            clips = await process_video(video_path, video_id)
+
+        update_supabase(url, video_id, video_path, clips)
+    finally:
+        if video_path:
+            os.remove(video_path)
+
+        for clip in clips:
+            os.remove(clip['path'])
+    return {
+        "url": url,
+        "video_id": video_id,
+        "video_path": video_path,
+        "clips": clips
+    }
